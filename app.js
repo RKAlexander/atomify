6754ca033e6350b1e1d8092fe1f6f0de829afa94
Ryan Alexander <rkalexander1@gmail.com>
2023-02-26 17:13:34 -0600

    add app update

--- app.js
+++ app.js
@@ -14,22 +14,21 @@ app.get('/', (req, res) => {
 });
 
 app.post('/scrape', (req, res) => {
-    const url = req.body.url;
-    request(url, (error, response, html) => {
-      if (!error && response.statusCode == 200) {
-        const dom = new JSDOM(html);
-        const h1 = dom.window.document.querySelector('h1').outerHTML;
-        const styles = getStylesByClassName(dom.window, 'c-page-header__title');
-        const stylesheetUrl = 'https://bradfrost.com/wp-content/themes/v8/style.css'; // replace with actual stylesheet URL
-        res.render('index', { h1, stylesheetUrl, styles });
-      } else {
-        res.render('index', { h1: `Error scraping ${url}` });
-      }
-    });
+  const url = req.body.url;
+  request(url, (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+      const dom = new JSDOM(html);
+      const h1 = dom.window.document.querySelector('h1').outerHTML;
+      const cssLinks = Array.from(dom.window.document.querySelectorAll('head link[rel="stylesheet"]')).map(link => link.outerHTML);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({ h1, cssLinks }));
+    } else {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({ h1: `Error scraping ${url}` }));
+    }
   });
-  
-  
-  
+});
+
 
 app.listen(3000, () => {
   console.log('App listening at http://localhost:3000');
