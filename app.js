1a67d46aee0ee30d9dffda4df12ff1c07c27785b
Ryan Alexander <rkalexander1@gmail.com>
2023-02-26 19:08:57 -0600

    h1 and h2 working

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// app.js
 const express = require('express');
 const request = require('request');
 const { JSDOM } = require('jsdom');
@@ -11,7 +12,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index', { h1: null, cssLinks: null });
+  res.render('index', { h1: null, h2s: null, cssLinks: null });
 });
 
 app.post('/scrape', (req, res) => {
@@ -20,12 +21,16 @@ app.post('/scrape', (req, res) => {
     if (!error && response.statusCode == 200) {
       const dom = new JSDOM(html);
       const h1 = dom.window.document.querySelector('h1').outerHTML;
+      const h2s = Array.from(dom.window.document.querySelectorAll('h2')).map(h2 => {
+        const style = dom.window.getComputedStyle(h2);
+        return { html: h2.outerHTML, style: style.cssText };
+      });
       const cssLinks = Array.from(dom.window.document.querySelectorAll('head link[rel="stylesheet"]')).map(link => ({
         href: link.getAttribute('href'),
         rel: link.getAttribute('rel')
       }));
       res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({ h1, cssLinks }));
+      res.send(JSON.stringify({ h1, h2s, cssLinks }));
     } else {
       res.setHeader('Content-Type', 'application/json');
       res.send(JSON.stringify({ h1: `Error scraping ${url}` }));
